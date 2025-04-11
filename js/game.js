a217795cd1b272f95113332528f8ea579cec0389
Domenic <domenic.dangelo@stu.ocsb.ca>
2025-04-11 17:26:51 +0000

    index game

---
+++ js/game.js
@@ -0,0 +1,32 @@
+/*global phaser*/
+
+// Copyright (c) 2025 domenic dangelo All rights reserved
+//
+// Created by: domenic dangelo
+// Created on: apr 2025
+// This file contains the JS functions for index.html
+
+/**
+ * Start phaser Game.
+ */
+const config = {
+  type: Phaser.AUTO,
+  width: 1920,
+  height: 1080,
+  physics: {
+    default: "arcade",
+    arcade: {
+      debug: true,
+    },
+  },
+  // set background color
+  backgroundColor: 0x5f6e7a,
+  scale: {
+    mode: Phaser.Scale.FIT,
+    // we place it in the middle of the page
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+  },
+}
+
+const game = new Phaser.Game(config)
+console.log(game)
