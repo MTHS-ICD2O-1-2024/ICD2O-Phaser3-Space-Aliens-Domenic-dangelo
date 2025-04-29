ce886724d76d27c17e4258d7bbde0685ca273bab
Domenic <domenic.dangelo@stu.ocsb.ca>
2025-04-29 17:07:36 +0000

    standard fix

--- js/game.js
+++ js/game.js
@@ -7,9 +7,9 @@
 // This file contains the JS functions for index.html
 
 // scene import statments
-import SplashScene from "./splashScene.js"
-import TitleScene from "./titleScene.js"
-import MenuScene from "./menuScene.js"
+import SplashScene from './splashScene.js'
+import TitleScene from './titleScene.js'
+import MenuScene from './menuScene.js'
 
 // create the new scenes
 const splashScene = new SplashScene()
@@ -25,7 +25,7 @@ const config = {
   width: 1920,
   height: 1080,
   physics: {
-    default: "arcade",
+    default: 'arcade',
     arcade: {
       debug: true
     }
@@ -42,9 +42,9 @@ const game = new Phaser.Game(config)
 
 // load scenes
 // Note: remember any "Key" is global and CAN NOT be reused!
-game.scene.add("splashScene", splashScene)
-game.scene.add("titleScene", titleScene)
-game.scene.add("menuScene", menuScene)
+game.scene.add('splashScene', splashScene)
+game.scene.add('titleScene', titleScene)
+game.scene.add('menuScene', menuScene)
 
 // the start scene
-game.scene.start("splashScene")
+game.scene.start('splashScene')
