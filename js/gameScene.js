e556c7b926f692152fb463e9fb34a4a8511b0609
Domenic <domenic.dangelo@stu.ocsb.ca>
2025-05-05 16:55:47 +0000

    standard fix gameScene

--- js/gameScene.js
+++ js/gameScene.js
@@ -48,8 +48,8 @@ class GameScene extends Phaser.Scene {
    * @param {object} data - any data passed via ScenePlugin.add() or ScenePlugin.start().
    */
   create (data) {
-      this.background = this.add.image(0,0, 'starBackground').setScale(2.0)
-    this.background.setOrigin(0,0)
+    this.background = this.add.image(0, 0, 'starBackground').setScale(2.0)
+    this.background.setOrigin(0, 0)
 
     this.ship = this.physics.add.sprite(1920 / 2, 1080 - 100, 'ship')
   }
